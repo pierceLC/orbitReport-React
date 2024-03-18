5a3bd8f6906f1050332fd0aff2aae49388776207
pierceLC <pierceford933@gmail.com>
2024-03-18 08:08:39 -0500

    pass local tests

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import satData from './satData';
+import './styling.css';
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
